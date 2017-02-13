30b8ae5ee61be45cd32ffe51572aae5a7b7f0353
SAHIL PAUDEL <sahilpaudel@yahoo.in>
2017-02-13 14:13:28 +0530

    Time For Design

--- app/src/main/java/com/sahilpaudel/app/advocatus/fragments/WriteRequestFragment.java
+++ app/src/main/java/com/sahilpaudel/app/advocatus/fragments/WriteRequestFragment.java
@@ -6,14 +6,17 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.jaredrummler.materialspinner.MaterialSpinner;
 import com.sahilpaudel.app.advocatus.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class WriteRequestFragment extends Fragment {
-
 
     public WriteRequestFragment() {
         // Required empty public constructor
@@ -24,7 +27,14 @@ public class WriteRequestFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_write_request, container, false);
+        View view = inflater.inflate(R.layout.fragment_write_request, container, false);
+
+        MaterialSpinner mCategory = (MaterialSpinner)view.findViewById(R.id.requestCategory);
+        EditText mPost = (EditText)view.findViewById(R.id.writepost);
+        EditText mStartTime = (EditText)view.findViewById(R.id.startTime);
+        EditText mEndTime = (EditText)view.findViewById(R.id.endTime);
+        Button mSubmit = (Button)view.findViewById(R.id.buttonSubmit);
+        return view;
     }
 
 }
